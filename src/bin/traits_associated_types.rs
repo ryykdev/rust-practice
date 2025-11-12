b953b4386413b3488a7c0d8a14a4d1c6662c84f8
Raphael <raphaelreichdev@gmail.com>
2025-11-12 10:15:24 +0100

    added marker traits

--- src/bin/traits_associated_types.rs
+++ src/bin/traits_associated_types.rs
@@ -1,13 +1,27 @@
 fn main() {
     // associated types
+    let my_struct = MyStruct { data: 42 };
+
+    println!("{}", my_struct.data);
+
+    println!("{}", my_struct.get_bar());
+}
+trait Foo {
+    type Bar<'a>
+    where
+        Self: 'a;
+
+    fn get_bar<'a>(&'a self) -> Self::Bar<'a>;
 }
 
-trait Container {
-    
-    type Foo;
-    type Bar;
+impl Foo for MyStruct {
+    type Bar<'a> = &'a i32;
 
-    get_bigger(&self) -> Option<F, B> {
-        
+    fn get_bar<'a>(&'a self) -> Self::Bar<'a> {
+        &self.data
     }
+}
+
+struct MyStruct {
+    data: i32,
 }
