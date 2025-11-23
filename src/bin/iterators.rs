fae170c6b36150f3e16b3947868119bbaf796287
Raphael <raphaelreichdev@gmail.com>
2025-11-23 18:20:38 +0100

    sync

--- src/bin/iterators.rs
+++ src/bin/iterators.rs
@@ -5,6 +5,15 @@ fn main() {
         "mandarin".to_string(),
         "watermelon".to_string(),
     ];
+    for fruit in fruits.iter_mut() {
+        if let Some(first_letter) = fruit.chars().next() {
+            let first = first_letter.to_uppercase();
+
+            let rest_of_word = &fruit[1..];
+
+            *fruit = first.to_string() + rest_of_word;
+        }
+    }
     let nuts = vec![
         "walnut".to_string(),
         "pistacio".to_string(),
@@ -13,15 +22,24 @@ fn main() {
     ];
 
     let foods = fruits.iter().chain(&nuts);
-
-    for food in foods {
-        println!("{food}");
+    for food in foods.clone() {
+        println!("chained: {food}");
+    }
+    // stepping over 2
+    for food in foods.step_by(2) {
+        println!("with step: {food}");
     }
 
     // iter mut
     for fruit in fruits.iter_mut() {
         *fruit = format!("fruits: {fruit}");
     }
+    dbg!(&fruits);
 
-    dbg!(fruits);
+    // iter mut
+    let nuts_prefixed = nuts.iter().map(|nut| format!("nut: {nut}"));
+    dbg!(&nuts_prefixed);
+
+    let zipped = fruits.iter().zip(&nuts);
+    zipped.for_each(|item: (_, _)| println!("first: {}, second: {}", item.0, item.1));
 }
